d8f9d6bdf3d21fc092b9f50c8857a0d831a77a16
Claeg Neander <111316828+claegneander@users.noreply.github.com>
2022-11-29 15:56:43 -0600

    Update Angelic_Helmet.java

--- src/main/java/me/claegneander/kori/item/sets/angelic/Angelic_Helmet.java
+++ src/main/java/me/claegneander/kori/item/sets/angelic/Angelic_Helmet.java
@@ -1,1 +1,28 @@
+package me.claegneander.kori.item.sets.angelic;
 
+import me.claegneander.kori.item.ItemBase;
+import me.claegneander.kori.item.Socket;
+import me.claegneander.kori.misc.enums.Color;
+import me.claegneander.kori.misc.enums.Tier;
+import me.claegneander.kori.rune.Runes;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+public class Angelic_Helmet extends ItemBase {
+
+    public Angelic_Helmet() {
+        super(Component.text("Angelic Helm").color(TextColor.fromHexString(Color.BLUE.getHEX())),
+                Material.NETHERITE_HELMET, Tier.GODLY, 0, true, true);
+    }
+    public ItemStack get(){
+        addLore(Component.text("Worn by the most beautiful princess of the Therans.").color(TextColor.fromHexString(Color.BLUE.getHEX())));
+        addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 10);
+        addEnchantment(Enchantment.DURABILITY, 10);
+      //addRune(Runes.ANGELIC_GRACE, 1);
+        setSockets(Socket.get(getTier()));
+        return create();
+    }
+}
